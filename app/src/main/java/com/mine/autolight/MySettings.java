9927d334416b0fe677fe780f3ac8cf52f5ec3ebb
The-First-King <59452275+The-First-King@users.noreply.github.com>
2025-12-21 09:31:50 -0500

    Change default mode from WORK_MODE_LANDSCAPE to WORK_MODE_UNLOCK

--- app/src/main/java/com/mine/autolight/MySettings.java
+++ app/src/main/java/com/mine/autolight/MySettings.java
@@ -24,7 +24,7 @@ public class MySettings {
 		b2 = sharedPref.getInt("b2", 30);
 		b3 = sharedPref.getInt("b3", 60);
 		b4 = sharedPref.getInt("b4", 90);
-		mode = sharedPref.getInt("mode", Constants.WORK_MODE_LANDSCAPE);
+		mode = sharedPref.getInt("mode", Constants.WORK_MODE_UNLOCK);
 	}
 
 	public void save() {
